319ab62d47dc14163ad505d98c3a5f4c6cfd2091
YolandaThant <yolanda.lee159@gmail.com>
2024-10-19 11:57:14 -0400

    Finished Task 2.2 of Phase 1

--- src/test/java/use_case/login/LoginInteractorTest.java
+++ src/test/java/use_case/login/LoginInteractorTest.java
@@ -15,6 +15,38 @@ public class LoginInteractorTest {
     // TODO Task 2.2: make a copy of this test method and follow the instructions in the readme to test your
     //                code from Task 2.1..
     @Test
+    public void successUserLoggedInTest() {
+        LoginInputData inputData = new LoginInputData("Paul", "password");
+        LoginUserDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
+
+        // For the success test, we need to add Paul to the data access repository before we log in.
+        UserFactory factory = new CommonUserFactory();
+        User user = factory.create("Paul", "password");
+        userRepository.save(user);
+
+        // Task 2.2.4: Before the use case is executed, the current user should initially be null.
+        assertNull(userRepository.getCurrentUser());
+
+        // This creates a successPresenter that tests whether the test case is as we expect.
+        LoginOutputBoundary successPresenter = new LoginOutputBoundary() {
+            @Override
+            public void prepareSuccessView(LoginOutputData user) {
+                assertEquals(userRepository.getCurrentUser(), user.getUsername());
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                fail("Use case failure is unexpected.");
+            }
+        };
+
+        LoginInputBoundary interactor = new LoginInteractor(userRepository, successPresenter);
+        // Task 2.2.4: Add an assertNull call just before the interactor.execute(inputData); call to make sure this is true.
+        assertNull(userRepository.getCurrentUser());
+        interactor.execute(inputData);
+    }
+
+    @Test
     public void successTest() {
         LoginInputData inputData = new LoginInputData("Paul", "password");
         LoginUserDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
